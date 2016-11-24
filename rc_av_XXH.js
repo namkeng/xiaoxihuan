e4a6540db5fa8e165c5c01b9e12489cc7799d732
namkeng <namkeng.tumblr@gmail.com>
2016-11-24 21:16:49 +0700

    Update rc_av_XXH.js

--- rc_av_XXH.js
+++ rc_av_XXH.js
@@ -8,4 +8,4 @@ document.writeln("<p class=\"rc_av_sub\">アナタ史上最高のオナニー体
 
 document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/F3BP5OaKBn6QpDTE?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/cb5QOpu.jpg\">");
-document.writeln("<p class=\"rc_av_sub\"天使もえ下着モデルをさせられて・・・ レンタル版</p></a>");
+document.writeln("<p class=\"rc_av_sub\">天使もえ下着モデルをさせられて・・・ レンタル版</p></a>");
