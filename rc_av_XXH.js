462b362e7f48cf13215f5655815a76dcae55f2f7
namkeng <namkeng.tumblr@gmail.com>
2016-11-24 22:58:08 +0700

    Update rc_av_XXH.js

--- rc_av_XXH.js
+++ rc_av_XXH.js
@@ -1,27 +1,27 @@
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/EHMPsSgILPPfYFNc?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/EHMPsSgILPPfYFNc?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/5pTqKo7.jpg\">");
 document.writeln("<p class=\"rc_av_sub\">天使もえ×ガチ童貞6名 超濃厚筆おろしサポート</p></a>");
 
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/STpH4MuSZA9dHTTw?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/STpH4MuSZA9dHTTw?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/mtlhdcJ.jpg\">");
-document.writeln("<p class=\"rc_av_sub\">アナタ史上最高のオナニー体験のために天使もえが200％...</p></a>");
+document.writeln("<p class=\"rc_av_sub\">アナタ史上最高のオナニー体験のために天使もえが200％全力...</p></a>");
 
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/F3BP5OaKBn6QpDTE?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/F3BP5OaKBn6QpDTE?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/cb5QOpu.jpg\">");
 document.writeln("<p class=\"rc_av_sub\">天使もえ下着モデルをさせられて・・・ レンタル版</p></a>");
 
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/r2X8zLitapofCs4C?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/r2X8zLitapofCs4C?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/eN4Ew3M.jpg\">");
-document.writeln("<p class=\"rc_av_sub\">露出したキャバ嬢の胸が刺激的で見とれていると彼女が気…</p></a>");
+document.writeln("<p class=\"rc_av_sub\">露出したキャバ嬢の胸が刺激的で見とれていると彼女が気づき...</p></a>");
 
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/tH4pVczw0RIIasiH?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/tH4pVczw0RIIasiH?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/ttqAyNu.jpg\">");
-document.writeln("<p class=\"rc_av_sub\">RION1ヵ月間セックスもオナニーも禁止されムラムラ全開...</p></a>");
+document.writeln("<p class=\"rc_av_sub\">1ヵ月間セックスもオナニーも禁止されムラムラ全開でアドレ...</p></a>");
 
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/3U42VAzwExzO4wAo?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/3U42VAzwExzO4wAo?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/q0a7ZLp.jpg\">");
 document.writeln("<p class=\"rc_av_sub\">凰かなめの極上筆おろし 10</p></a>");
 
-document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/gOqBLANgzMc263AE?src=xiaoxihuan\" target=\"_blank\">");
+document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/gOqBLANgzMc263AE?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/Zgr4xJW.jpg\">");
 document.writeln("<p class=\"rc_av_sub\">紗倉まな　焦らして高まる、媚薬ＳＥＸ</p></a>");
