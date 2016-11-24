74ef5ee07c7546739b40c1f8a3cb9f0fc58c04a3
namkeng <namkeng.tumblr@gmail.com>
2016-11-24 22:16:23 +0700

    Update rc_av_XXH.js

--- rc_av_XXH.js
+++ rc_av_XXH.js
@@ -4,7 +4,7 @@ document.writeln("<p class=\"rc_av_sub\">天使もえ×ガチ童貞6名 超濃�
 
 document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/STpH4MuSZA9dHTTw?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/mtlhdcJ.jpg\">");
-document.writeln("<p class=\"rc_av_sub\">アナタ史上最高のオナニー体験のために天使もえが200％全力...</p></a>");
+document.writeln("<p class=\"rc_av_sub\">アナタ史上最高のオナニー体験のために天使もえが200％...</p></a>");
 
 document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8porn.com/v/F3BP5OaKBn6QpDTE?src=xiaoxihuan\" target=\"_blank\">");
 document.writeln("<img width=\"200px\" src=\"http://i.imgur.com/cb5QOpu.jpg\">");
