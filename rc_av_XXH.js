b80db62f2764f7cc1964d3c7f988c0bce65cf539
namkeng <namkeng.tumblr@gmail.com>
2016-11-26 17:27:16 +0700

    Add files via upload

---
+++ rc_av_XXH.js
@@ -0,0 +1,25 @@
+var vd_data= [
+  ['EHMPsSgILPPfYFNc','http://i.imgur.com/5pTqKo7.jpg','天使もえ×ガチ童貞6名 超濃厚筆おろしサポート'],
+  ['STpH4MuSZA9dHTTw','http://i.imgur.com/mtlhdcJ.jpg','アナタ史上最高のオナニー体験のために天使もえが200％全力...'],
+  ['F3BP5OaKBn6QpDTE','http://i.imgur.com/cb5QOpu.jpg','天使もえ下着モデルをさせられて・・・ レンタル版'],
+  ['r2X8zLitapofCs4C','http://i.imgur.com/eN4Ew3M.jpg','露出したキャバ嬢の胸が刺激的で見とれていると彼女が気づき...'],
+  ['tH4pVczw0RIIasiH','http://i.imgur.com/ttqAyNu.jpg','1ヵ月間セックスもオナニーも禁止されムラムラ全開でアドレ...'],
+  ['3U42VAzwExzO4wAo','http://i.imgur.com/q0a7ZLp.jpg','凰かなめの極上筆おろし 10'],
+  ['gOqBLANgzMc263AE','http://i.imgur.com/Zgr4xJW.jpg','紗倉まな　焦らして高まる、媚薬ＳＥＸ'],
+  ['Li8QIbGMjsyY8zLk','http://i.imgur.com/hl29Hoc.jpg','紗倉まな 媚薬催眠トランス大絶頂セックス レンタル版'],
+  ['q0XtNR45iwuLwDGw','http://i.imgur.com/JeG4TlO.jpg','愛犬が美少女に大変身！ 擬人化ペット天使もえ...'],
+  ['pL0nIz4AaKZaGY05','http://i.imgur.com/gNgXhcY.jpg','唾液ダラダラ接吻中毒ベロキス性交　天使もえ'],
+];
+
+function randomsort(a, b) {
+    return Math.random()>.5 ? -1 : 1;
+    //用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
+}
+
+vd_data.sort(randomsort);
+
+for(i=0;i<100;i++){
+  document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/"+vd_data[i][0]+"?src=xiaoxihuan\" target=\"_blank\">");
+  document.writeln("<img width=\"200px\" src=\""+vd_data[i][1]+"\">");
+  document.writeln("<p class=\"rc_av_sub\">"+vd_data[i][2]+"</p></a>");
+}
