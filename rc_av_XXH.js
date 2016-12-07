7e5d19100ec4f5fe27ca10b98fe4dfeb1eb92fab
namkeng <namkeng.tumblr@gmail.com>
2016-12-07 09:11:55 +0700

    Update rc_av_XXH.js

--- rc_av_XXH.js
+++ rc_av_XXH.js
@@ -1,27 +1,28 @@
 var vd_data= [
-  ['EHMPsSgILPPfYFNc','http://i.imgur.com/5pTqKo7.jpg','天使もえ×ガチ童貞6名 超濃厚筆おろしサポート'],
-  ['STpH4MuSZA9dHTTw','http://i.imgur.com/mtlhdcJ.jpg','アナタ史上最高のオナニー体験のために天使もえが200％全力...'],
-  ['F3BP5OaKBn6QpDTE','http://i.imgur.com/cb5QOpu.jpg','天使もえ下着モデルをさせられて・・・ レンタル版'],
-  ['r2X8zLitapofCs4C','http://i.imgur.com/eN4Ew3M.jpg','露出したキャバ嬢の胸が刺激的で見とれていると彼女が気づき...'],
-  ['tH4pVczw0RIIasiH','http://i.imgur.com/ttqAyNu.jpg','1ヵ月間セックスもオナニーも禁止されムラムラ全開でアドレ...'],
-  ['3U42VAzwExzO4wAo','http://i.imgur.com/q0a7ZLp.jpg','凰かなめの極上筆おろし 10'],
-  ['gOqBLANgzMc263AE','http://i.imgur.com/Zgr4xJW.jpg','紗倉まな　焦らして高まる、媚薬ＳＥＸ'],
-  ['Li8QIbGMjsyY8zLk','http://i.imgur.com/hl29Hoc.jpg','紗倉まな 媚薬催眠トランス大絶頂セックス レンタル版'],
-  ['q0XtNR45iwuLwDGw','http://i.imgur.com/JeG4TlO.jpg','愛犬が美少女に大変身！ 擬人化ペット天使もえ...'],
-  ['pL0nIz4AaKZaGY05','http://i.imgur.com/gNgXhcY.jpg','唾液ダラダラ接吻中毒ベロキス性交　天使もえ'],
+  ['http://www.yingtaopa.com/v/EHMPsSgILPPfYFNc?src=xiaoxihuan','http://i.imgur.com/5pTqKo7.jpg','天使もえ×ガチ童貞6名 超濃厚筆おろしサポート'],
+  ['http://www.yingtaopa.com/v/STpH4MuSZA9dHTTw?src=xiaoxihuan','http://i.imgur.com/mtlhdcJ.jpg','アナタ史上最高のオナニー体験のために天使もえが200％全力...'],
+  ['http://www.yingtaopa.com/v/F3BP5OaKBn6QpDTE?src=xiaoxihuan','http://i.imgur.com/cb5QOpu.jpg','天使もえ下着モデルをさせられて・・・ レンタル版'],
+  ['http://www.yingtaopa.com/v/r2X8zLitapofCs4C?src=xiaoxihuan','http://i.imgur.com/eN4Ew3M.jpg','露出したキャバ嬢の胸が刺激的で見とれていると彼女が気づき...'],
+  ['http://www.yingtaopa.com/v/tH4pVczw0RIIasiH?src=xiaoxihuan','http://i.imgur.com/ttqAyNu.jpg','1ヵ月間セックスもオナニーも禁止されムラムラ全開でアドレ...'],
+  ['http://www.yingtaopa.com/v/3U42VAzwExzO4wAo?src=xiaoxihuan','http://i.imgur.com/q0a7ZLp.jpg','凰かなめの極上筆おろし 10'],
+  ['http://www.yingtaopa.com/v/gOqBLANgzMc263AE?src=xiaoxihuan','http://i.imgur.com/Zgr4xJW.jpg','紗倉まな　焦らして高まる、媚薬ＳＥＸ'],
+  ['http://www.yingtaopa.com/v/Li8QIbGMjsyY8zLk?src=xiaoxihuan','http://i.imgur.com/hl29Hoc.jpg','紗倉まな 媚薬催眠トランス大絶頂セックス レンタル版'],
+  ['http://www.yingtaopa.com/v/q0XtNR45iwuLwDGw?src=xiaoxihuan','http://i.imgur.com/JeG4TlO.jpg','愛犬が美少女に大変身！ 擬人化ペット天使もえ...'],
+  ['http://www.yingtaopa.com/v/pL0nIz4AaKZaGY05?src=xiaoxihuan','http://i.imgur.com/gNgXhcY.jpg','唾液ダラダラ接吻中毒ベロキス性交　天使もえ'],
 
-  ['VWbJZ0xuUijhPtN7','http://i.imgur.com/YnO77bw.jpg','彼女のお姉さんは、誘惑ヤリたがり娘。桃谷エリカ'],
-  ['I1u2IMBZNOvUzi4Y','http://i.imgur.com/dML0OQP.jpg','快感スプラッシュ！はじめての気持ち良すぎる潮吹き 三上悠亜'],
-  ['QnnmqDnYlBvKBVWC','http://i.imgur.com/SGLmR4z.jpg','犯された巨乳女子校生 美竹すず 校内でぶっかけ輪姦された優等生'],
-  ['N4gBQje7NBU9cmmg','http://i.imgur.com/IYe1nwX.jpg','完全緊縛されて無理やり犯された巨乳若妻 葵'],
-  ['by6rK29klV1xeyEC','http://i.imgur.com/MXtC4c7.jpg','ザーメン大好き天使もえ'],
-  ['gvstUxOf290b0M7M','http://i.imgur.com/OTOscu1.jpg','おま●こ、くぱぁ。　天使もえ'],
-  ['83e92eMbrlWTbw6P','http://i.imgur.com/tCVJVMN.jpg','あやみ旬果 身動き出来ない美少女をひたすらイカせまくる拘束性交'],
-  ['UFnHBJmgfMwIhzW3','http://i.imgur.com/oWUOhtW.jpg','長谷川るい 濃厚密写 接写エロティシズム3本番'],
-  ['QxeRZ56ljZhJ6EqT','http://i.imgur.com/qoDSoKw.jpg','白石茉莉奈 息子の同級生を逆レイプ 乗っかり種搾りプレス'],
-  ['7mmm9h5hTOOPQoyM','http://i.imgur.com/iPzjwA8.jpg','古川いおり むきだし'],
-  ['AcaZQYivFn8oqezh','http://i.imgur.com/yNWF47a.jpg','夢乃あいか 常に全身ローションだっくだくのご奉仕ぬるぬる爆乳...'],
-  ['6jJwMhprMfcU0Kkw','http://i.imgur.com/olQ4Bl9.jpg','盗撮リアルドキュメント 密着50日、天使もえのプライベートを...'],
+  ['http://www.yingtaopa.com/v/VWbJZ0xuUijhPtN7?src=xiaoxihuan','http://i.imgur.com/YnO77bw.jpg','彼女のお姉さんは、誘惑ヤリたがり娘。桃谷エリカ'],
+  ['http://www.yingtaopa.com/v/I1u2IMBZNOvUzi4Y?src=xiaoxihuan','http://i.imgur.com/dML0OQP.jpg','快感スプラッシュ！はじめての気持ち良すぎる潮吹き 三上悠亜'],
+  ['http://www.yingtaopa.com/v/QnnmqDnYlBvKBVWC?src=xiaoxihuan','http://i.imgur.com/SGLmR4z.jpg','犯された巨乳女子校生 美竹すず 校内でぶっかけ輪姦された優等生'],
+  ['http://www.yingtaopa.com/v/N4gBQje7NBU9cmmg?src=xiaoxihuan','http://i.imgur.com/IYe1nwX.jpg','完全緊縛されて無理やり犯された巨乳若妻 葵'],
+  ['http://www.yingtaopa.com/v/by6rK29klV1xeyEC?src=xiaoxihuan','http://i.imgur.com/MXtC4c7.jpg','ザーメン大好き天使もえ'],
+  ['http://www.yingtaopa.com/v/gvstUxOf290b0M7M?src=xiaoxihuan','http://i.imgur.com/OTOscu1.jpg','おま●こ、くぱぁ。　天使もえ'],
+  ['http://www.yingtaopa.com/v/83e92eMbrlWTbw6P?src=xiaoxihuan','http://i.imgur.com/tCVJVMN.jpg','あやみ旬果 身動き出来ない美少女をひたすらイカせまくる拘束性交'],
+  ['http://www.yingtaopa.com/v/UFnHBJmgfMwIhzW3?src=xiaoxihuan','http://i.imgur.com/oWUOhtW.jpg','長谷川るい 濃厚密写 接写エロティシズム3本番'],
+  ['http://www.yingtaopa.com/v/QxeRZ56ljZhJ6EqT?src=xiaoxihuan','http://i.imgur.com/qoDSoKw.jpg','白石茉莉奈 息子の同級生を逆レイプ 乗っかり種搾りプレス'],
+  ['http://www.yingtaopa.com/v/7mmm9h5hTOOPQoyM?src=xiaoxihuan','http://i.imgur.com/iPzjwA8.jpg','古川いおり むきだし'],
+  ['http://www.yingtaopa.com/v/AcaZQYivFn8oqezh?src=xiaoxihuan','http://i.imgur.com/yNWF47a.jpg','夢乃あいか 常に全身ローションだっくだくのご奉仕ぬるぬる爆乳...'],
+  ['http://www.yingtaopa.com/v/6jJwMhprMfcU0Kkw?src=xiaoxihuan','http://i.imgur.com/olQ4Bl9.jpg','盗撮リアルドキュメント 密着50日、天使もえのプライベートを...'],
+  ['http://www.5genvpiao.com/?src=xiaoxihuan','http://i.imgur.com/syFUFFz.jpg','<b>五姑娘导航，好用的成人导航，一个就够！</b>'],
 
 ];
 
@@ -33,7 +34,7 @@ function randomsort(a, b) {
 vd_data.sort(randomsort);
 
 for(i=0;i<100;i++){
-  document.writeln("<a class=\"rc_av_link\" href=\"http://www.yingtaopa.com/v/"+vd_data[i][0]+"?src=xiaoxihuan\" target=\"_blank\">");
+  document.writeln("<a class=\"rc_av_link\" href=\"+"vd_data[i][0]+"\" target=\"_blank\">");
   document.writeln("<img width=\"200px\" src=\""+vd_data[i][1]+"\">");
   document.writeln("<p class=\"rc_av_sub\">"+vd_data[i][2]+"</p></a>");
 }
