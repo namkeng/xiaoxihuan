422dbf068e9d2c560f1aaac8052d0d1810dfa2ea
namkeng <namkeng.tumblr@gmail.com>
2016-12-07 08:36:30 +0700

    Update rc_av_XXH.js

--- rc_av_XXH.js
+++ rc_av_XXH.js
@@ -1,15 +1,15 @@
 var vd_data= [
   ['EHMPsSgILPPfYFNc','http://i.imgur.com/5pTqKo7.jpg','天使もえ×ガチ童貞6名 超濃厚筆おろしサポート'],
-  ['STpH4MuSZA9dHTTw','http://i.imgur.com/mtlhdcJ.jpg','アナタ史上最高のオナニー体験のために天使もえが200％全力でちんしこサポート 夢の...'],
+  ['STpH4MuSZA9dHTTw','http://i.imgur.com/mtlhdcJ.jpg','アナタ史上最高のオナニー体験のために天使もえが200％全力...'],
   ['F3BP5OaKBn6QpDTE','http://i.imgur.com/cb5QOpu.jpg','天使もえ下着モデルをさせられて・・・ レンタル版'],
-  ['r2X8zLitapofCs4C','http://i.imgur.com/eN4Ew3M.jpg','露出したキャバ嬢の胸が刺激的で見とれていると彼女が気づき、やたらと目を合わせて...'],
-  ['tH4pVczw0RIIasiH','http://i.imgur.com/ttqAyNu.jpg','RION 1ヵ月間セックスもオナニーも禁止されムラムラ全開でアドレナリン爆発！痙攣しま...'],
+  ['r2X8zLitapofCs4C','http://i.imgur.com/eN4Ew3M.jpg','露出したキャバ嬢の胸が刺激的で見とれていると彼女が気づき...'],
+  ['tH4pVczw0RIIasiH','http://i.imgur.com/ttqAyNu.jpg','1ヵ月間セックスもオナニーも禁止されムラムラ全開でアドレ...'],
   ['3U42VAzwExzO4wAo','http://i.imgur.com/q0a7ZLp.jpg','凰かなめの極上筆おろし 10'],
   ['gOqBLANgzMc263AE','http://i.imgur.com/Zgr4xJW.jpg','紗倉まな　焦らして高まる、媚薬ＳＥＸ'],
   ['Li8QIbGMjsyY8zLk','http://i.imgur.com/hl29Hoc.jpg','紗倉まな 媚薬催眠トランス大絶頂セックス レンタル版'],
-  ['q0XtNR45iwuLwDGw','http://i.imgur.com/JeG4TlO.jpg','愛犬が美少女に大変身！ 擬人化ペット天使もえの異常な恩返し交尾 独身男が発情ワン娘...'],
+  ['q0XtNR45iwuLwDGw','http://i.imgur.com/JeG4TlO.jpg','愛犬が美少女に大変身！ 擬人化ペット天使もえ...'],
   ['pL0nIz4AaKZaGY05','http://i.imgur.com/gNgXhcY.jpg','唾液ダラダラ接吻中毒ベロキス性交　天使もえ'],
-  
+
   ['VWbJZ0xuUijhPtN7','http://i.imgur.com/YnO77bw.jpg','彼女のお姉さんは、誘惑ヤリたがり娘。桃谷エリカ'],
   ['I1u2IMBZNOvUzi4Y','http://i.imgur.com/dML0OQP.jpg','快感スプラッシュ！はじめての気持ち良すぎる潮吹き 三上悠亜'],
   ['QnnmqDnYlBvKBVWC','http://i.imgur.com/SGLmR4z.jpg','犯された巨乳女子校生 美竹すず 校内でぶっかけ輪姦された優等生'],
@@ -20,6 +20,9 @@ var vd_data= [
   ['UFnHBJmgfMwIhzW3','http://i.imgur.com/oWUOhtW.jpg','長谷川るい 濃厚密写 接写エロティシズム3本番'],
   ['QxeRZ56ljZhJ6EqT','http://i.imgur.com/qoDSoKw.jpg','白石茉莉奈 息子の同級生を逆レイプ 乗っかり種搾りプレス'],
   ['7mmm9h5hTOOPQoyM','http://i.imgur.com/iPzjwA8.jpg','古川いおり むきだし'],
+  ['AcaZQYivFn8oqezh','http://i.imgur.com/yNWF47a.jpg','夢乃あいか 常に全身ローションだっくだくのご奉仕ぬるぬる爆乳...'],
+  ['6jJwMhprMfcU0Kkw','http://i.imgur.com/olQ4Bl9.jpg','盗撮リアルドキュメント 密着50日、天使もえのプライベートを...'],
+
 ];
 
 function randomsort(a, b) {
@@ -30,7 +33,7 @@ function randomsort(a, b) {
 vd_data.sort(randomsort);
 
 for(i=0;i<100;i++){
-  document.writeln("<a class=\"rc_av_link\" href=\"http://www.d8lulu.com/v/"+vd_data[i][0]+"?src=xiaoxihuan\" target=\"_blank\">");
+  document.writeln("<a class=\"rc_av_link\" href=\"http://www.yingtaopa.com/v/"+vd_data[i][0]+"?src=xiaoxihuan\" target=\"_blank\">");
   document.writeln("<img width=\"200px\" src=\""+vd_data[i][1]+"\">");
   document.writeln("<p class=\"rc_av_sub\">"+vd_data[i][2]+"</p></a>");
 }
